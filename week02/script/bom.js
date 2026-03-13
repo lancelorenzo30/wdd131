fd4efd13d03f1c07add9e4977e880469bd9aa3c0
Lance Lorenzo <rozenkranlorenzo1730@gmail.com>
2026-03-13 20:25:03 +0800

    book of mormon activity

--- week02/script/bom.js
+++ week02/script/bom.js
@@ -1,12 +1,25 @@
-const input = document.querySelector("#favchamp");
+const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-const li = document.createElement("li");
-li.textContent = input.value;
-const deleteBtn = document.createElement("button");
-deleteBtn.textContent = "❌";
 
-list.appendChild(li);
+button.addEventListener("click", function () {
+    if (input.value.trim() != "") {
+        const li = document.createElement("li");
+        li.textContent = input.value;
 
-input.focus();
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "❌";
+        deleteBtn.addEventListener("click", function () {
+            list.removeChild(li);
+            input.focus();
+        })
+
+        li.appendChild(deleteBtn);
+
+        list.appendChild(li);
+
+        input.value = "";
+    }
+    input.focus()
+})
