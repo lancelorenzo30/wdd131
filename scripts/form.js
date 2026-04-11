08fba42b115d7fb7bc3206150f5141969f598261
Lance Lorenzo <rozenkranlorenzo1730@gmail.com>
2026-04-12 01:07:15 +0800

    Week05 product review form complete

---
+++ scripts/form.js
@@ -0,0 +1,58 @@
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const productSelect = document.getElementById("product");
+
+    products.forEach(product => {
+        const option = document.createElement("option");
+
+        option.value = product.id;
+        option.textContent = product.name;
+
+        productSelect.appendChild(option);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    let reviewCount = localStorage.getItem("reviewCount");
+
+    if (reviewCount === null) {
+        reviewCount = 0;
+    }
+
+    reviewCount = Number(reviewCount) + 1;
+
+    localStorage.setItem("reviewCount", reviewCount);
+
+    const display = document.getElementById("reviewCount");
+
+    if (display) {
+        display.textContent = `You have submitted ${reviewCount} review(s).`;
+    };
+});
